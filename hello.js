1c60efd7d9d61d45487f7450dc9beac61e6a3e25
kondo takeshi <chaspy@users.noreply.github.com>
2022-08-10 12:33:44 +0900

    hello

--- hello.js
+++ hello.js
@@ -7,3 +7,15 @@ var a = {
 var foo = [0, 1, 2, 3];
 foo.push(4);
 console.log(foo);
+var f = func;
+function func(arg) {
+    return Number(arg);
+}
+var fuuunc = function (boo) {
+    var bar = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        bar[_i - 1] = arguments[_i];
+    }
+    return bar;
+};
+console.log(fuuunc(1, 2, 3));
--- hello.ts
+++ hello.ts
@@ -14,3 +14,12 @@ const a: MyObj = {
 const foo: number[] = [0, 1, 2, 3];
 foo.push(4);
 console.log(foo);
+
+const f: (foo: string) => number = func;
+
+function func(arg: string): number {
+  return Number(arg);
+}
+
+const fuuunc = (one: string, ...two: number[]) => two;
+console.log(fuuunc("hey", 2, 3));
