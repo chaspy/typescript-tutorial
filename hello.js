9f244f9413ca33ff5519102ead74160e53ee414a
kondo takeshi <chaspy@users.noreply.github.com>
2022-08-10 12:17:31 +0900

    hello

--- hello.js
+++ hello.js
@@ -1,2 +1,9 @@
-var message = 'Hello! TypeScript!';
+var message = "Hello! TypeScript!";
 console.log(message);
+var a = {
+    foo: "foo",
+    bar: 1
+};
+var foo = [0, 1, 2, 3];
+foo.push(4);
+console.log(foo);
--- hello.ts
+++ hello.ts
@@ -1,2 +1,16 @@
-const message:string = 'Hello! TypeScript!';
+const message: string = "Hello! TypeScript!";
 console.log(message);
+
+interface MyObj {
+  foo: string;
+  bar: number;
+}
+
+const a: MyObj = {
+  foo: "foo",
+  bar: 1,
+};
+
+const foo: number[] = [0, 1, 2, 3];
+foo.push(4);
+console.log(foo);
