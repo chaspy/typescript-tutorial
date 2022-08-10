2cef928f81ed5b2a9c5aa845c52c4295d57eb7f0
kondo takeshi <chaspy@users.noreply.github.com>
2022-08-10 23:46:48 +0900

    union

---
+++ union.js
@@ -0,0 +1,8 @@
+function useHogePiyo(obj) {
+    if ("bar" in obj) {
+        console.log("Hoge", obj.bar);
+    }
+    else {
+        console.log("Piyo", obj.baz);
+    }
+}
---
+++ union.ts
@@ -0,0 +1,17 @@
+interface Hoge {
+  foo: string;
+  bar: number;
+}
+
+interface Piyo {
+  foo: number;
+  baz: boolean;
+}
+
+function useHogePiyo(obj: Hoge | Piyo): void {
+  if ("bar" in obj) {
+    console.log("Hoge", obj.bar);
+  } else {
+    console.log("Piyo", obj.baz);
+  }
+}
